8d45b5eaa7a768d3e26d318227a4173ab2cc3767
Frederik Wulf <frederikw@compusoftgroup.com>
2023-04-21 20:32:52 +0200

    feature: add decoding of jwt

--- src/app/services/jwt/jwt.service.ts
+++ src/app/services/jwt/jwt.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { LocalStorageService } from '../local-storage/local-storage.service';
+import jwt_decode from "jwt-decode";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,12 @@ export class JwtService {
 
   private _jwt?: string;
 
+  public decodedJwt?: {
+    email: string;
+    customerId: string;
+    role: string;
+    expiring: Date
+  };
 
   get jwt(): string | undefined {
     if (!this._jwt) {
@@ -20,6 +27,9 @@ export class JwtService {
 
   set jwt(value: string | undefined) {
     this._jwt = value;
+    if (typeof value === "string") {
+      this.setParsedJwt(value);
+    }
     this.saveJwtToLocalStorage();
   }
 
@@ -27,10 +37,21 @@ export class JwtService {
   }
 
   private loadJtwFromLocalStorage() {
-    this._jwt = this.localStorage.getItem(this.localStorageKey) || undefined;
+    this.jwt = this.localStorage.getItem(this.localStorageKey) || undefined;
   }
 
   saveJwtToLocalStorage() {
     this.localStorage.setItem(this.localStorageKey, this.jwt as string);
   }
+
+  private setParsedJwt(jwt: string) {
+    const decoded: any = jwt_decode(jwt);
+    this.decodedJwt = {
+        expiring: new Date(decoded.exp * 1000),
+        email: decoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'],
+        customerId: decoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'],
+        role: decoded['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']
+      }
+  }
+
 }
