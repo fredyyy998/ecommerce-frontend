904e2c3cf9f2b0fedcb7fbc052840926bc306bc0
Frederik Wulf <frederikw@compusoftgroup.com>
2023-04-30 16:21:02 +0200

    feature: add build setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrlAccountService: 'http://localhost:8081',
+  baseUrlShoppingCartService: 'http://localhost:8082',
+  baseUrlInventoryService: 'http://localhost:8080',
+  baseUrlFulfillmentService: 'http://localhost:8083',
 };
